95b0c304dafa57a39256bb32769b588021c6b0b9
Selmaan <chettih@fas.harvard.edu>
2015-12-15 14:04:55 -0500

    added scPDF

---
+++ scPDF.m
@@ -0,0 +1,6 @@
+function scPDF(fileName)
+
+set(gcf,'Units','Inches');
+pos = get(gcf,'Position');
+set(gcf,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(gcf,fileName,'-dpdf','-r0')
